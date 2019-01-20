c8c738c0b0eae9e2c38670ceeac58ca61782daca
MagnetoWang <1527672216@qq.com>
2019-01-20 15:44:57 +0800

    Create test_map.cpp

---
+++ ibdb/src/test/test_map.cpp
@@ -0,0 +1,17 @@
+//
+// Created by magnetowang on 2018/12/21.
+//
+#include <stdio.h>
+#include <iostream>
+#include <map>
+
+int main() {
+    std::map<int, std::string> sort_key;
+    sort_key.insert(std::make_pair(1000, "baidu"));
+    sort_key.insert(std::make_pair(500, "ali"));
+    sort_key.insert(std::make_pair(700, "tengxu"));
+    for (const auto& iter : sort_key) {
+        std::cout<<iter.first<<std::endl;
+        std::cout<<iter.second<<std::endl;
+    }
+}
