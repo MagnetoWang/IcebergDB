7629898aad505b9ded67a00f6788412bc618f1ac
MagnetoWang <1527672216@qq.com>
2019-01-31 21:05:05 +0800

    增加boost测试用例

---
+++ ibdb/src/test/boost_test.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file boost_test.cpp
+ * @author MagnetoWang
+ * @brief 
+ * @version 0.1
+ * @date 2019-01-30
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+#include <iostream>
+#include <boost/version.hpp>
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Boost版本：" << BOOST_VERSION << std::endl;
+    return 0;
+}
+
+
+
