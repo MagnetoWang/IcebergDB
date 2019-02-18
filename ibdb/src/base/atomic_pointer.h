c05cd66cbf54a24f41c2e8178f50651d8457f969
MagnetoWang <1527672216@qq.com>
2019-02-18 11:10:45 +0800

    Create atomic_pointer.h

---
+++ ibdb/src/base/atomic_pointer.h
@@ -0,0 +1,36 @@
+#ifndef IBDB_BASE_ATOMIC_POINTER_H
+#define IBDB_BASE_ATOMIC_POINTER_H
+#include <stdint.h>
+#include <atomic>
+
+namespace ibdb {
+
+namespace base {
+
+class AtomicPointer {
+public:
+    AtomicPointer() {}
+    explicit AtomicPointer(void* v) : rep_(v) {}
+    inline void* AcquireLoad() const {
+        return rep_.load(std::memory_order_acquire);
+    }
+    inline void ReleaseStore(void* v) {
+        rep_.store(v, std::memory_order_release);
+    }
+    inline void* NoBarrierLoad() {
+        return rep_.load(std::memory_order_relaxed);
+    }
+    inline void NoBarrierStore(void* v) {
+        rep_.store(v, std::memory_order_relaxed);
+    }
+private:
+    std::atomic<void*> rep_;
+};
+
+
+} // base
+} // ibdb
+
+
+
+#endif // IBDB_BASE_ATOMIC_POINTER_H
