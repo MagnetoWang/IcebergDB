272a00e5b7c67ce03b4847b71c7ec7ce6bf667db
MagnetoWang <1527672216@qq.com>
2019-03-03 22:37:45 +0800

    Update port.h

--- ibdb/src/port/port.h
+++ ibdb/src/port/port.h
@@ -4,8 +4,6 @@
  * @brief 跨平台相关代码
  * 
  */
-
-
 typedef signed char           int8_t;
 typedef signed short          int16_t;
 typedef signed int            int32_t;
@@ -14,6 +12,21 @@ typedef unsigned char         uint8_t;
 typedef unsigned short        uint16_t;
 typedef unsigned int          uint32_t;
 typedef unsigned long long    uint64_t;
+namespace ibdb {
+namespace port {
 
+inline const bool IsLittleEndian() {
+    int a = 1;
+    if (*(char*)&a == 1) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+static const bool kLittleEndian = IsLittleEndian();
+
+}
+}
 
 #endif // IBDB_PORT_PORT_H
