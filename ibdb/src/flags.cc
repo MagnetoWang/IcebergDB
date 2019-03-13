645d85f2fad96d06fa3798bca03f0bf313e86cf7
MagnetoWang <1527672216@qq.com>
2019-03-13 18:13:38 +0800

    Update flags.cc

--- ibdb/src/flags.cc
+++ ibdb/src/flags.cc
@@ -2,4 +2,9 @@
 // Test GFlags
 DEFINE_string(test_flags, "test test test", "config the ip and port that rtidb serves for");
 
-
+//Test GLog
+DEFINE_string(ibdb_log_dir, "/Users/magnetowang/Documents/GitHub/IcebergDB/ibdb/log/", "config log dir for testing");
+DEFINE_string(log_dir_info, "/Users/magnetowang/Documents/GitHub/IcebergDB/ibdb/log/info", "config log dir for testing");
+DEFINE_string(log_dir_warn, "/Users/magnetowang/Documents/GitHub/IcebergDB/ibdb/log/warn", "config log dir for testing");
+DEFINE_string(log_dir_error, "/Users/magnetowang/Documents/GitHub/IcebergDB/ibdb/log/error", "config log dir for testing");
+DEFINE_string(log_dir_fatal, "/Users/magnetowang/Documents/GitHub/IcebergDB/ibdb/log/fatal", "config log dir for testing");
