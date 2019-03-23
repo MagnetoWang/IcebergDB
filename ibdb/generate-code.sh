6b9dfb711a60fd40df3f9d22f4116d29d6922af3
MagnetoWang <1527672216@qq.com>
2019-03-23 20:18:01 +0800

    Update generate-code.sh

--- ibdb/generate-code.sh
+++ ibdb/generate-code.sh
@@ -2,6 +2,13 @@ ROOT_PATH=`pwd`
 
 PROTOC_PATH=$ROOT_PATH/src/protobuf
 
+# third-party/bin/protoc --proto_path=$PROTOC_PATH --cpp_out=$PROTOC_PATH $PROTOC_PATH/storage.proto
+# third-party/bin/protoc --version
+
+# be careful protoc must be v2.6.0 or lower version
 protoc --proto_path=$PROTOC_PATH --cpp_out=$PROTOC_PATH storage.proto
 
 cd $ROOT_PATH
+
+# third-party/bin/protoc --proto_path=/Users/magnetowang/Documents/GitHub/IcebergDB/ibdb/src/protobuf --cpp_out=/Users/magnetowang/Documents/GitHub/IcebergDB/ibdb/src/protobuf storage.proto
+
