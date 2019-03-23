ca07abc78eced49648e35159592f9f748420dc8a
MagnetoWang <1527672216@qq.com>
2019-03-23 20:18:16 +0800

    Update run.sh

--- ibdb/run.sh
+++ ibdb/run.sh
@@ -3,16 +3,16 @@ echo run this $1 file
 
 # cp -r third-party/lib/. build
 cd build
-
+mkdir -p reports
 if [ $1 = 'all' ]; then
         rows=$(ls | grep test | awk '{print $1}')
         echo $rows
         for test in $rows
         do
-            ./$test
+            ./$test --gtest_output=xml:./reports/$test.xml
         done
     else
-        ./$1
+        ./$1 --gtest_output=xml:./reports/$1.xml
 fi
 
 # for i in {1..2}
