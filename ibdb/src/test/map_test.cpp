c0e72b90febff53f4762520e1c4a9e446e9d67fa
MagnetoWang <1527672216@qq.com>
2019-04-09 13:56:49 +0800

    验证测试：map的key按字典序和数字从小到大的顺序

--- ibdb/src/test/map_test.cpp
+++ ibdb/src/test/map_test.cpp
@@ -15,4 +15,14 @@ int main() {
         std::cout<<iter.first<<std::endl;
         // std::cout<<iter.second<<std::endl;
     }
+    std::map<std::string, int> string_key;
+    string_key.insert(std::make_pair("abc", 10));
+    string_key.insert(std::make_pair("bbb", 10));
+    string_key.insert(std::make_pair("aaa", 10));
+    string_key.insert(std::make_pair("ccc", 10));
+    string_key.insert(std::make_pair("abcd", 10));
+    string_key.insert(std::make_pair("abcde", 10));
+    for (const auto& iter : string_key) {
+        std::cout<<iter.first<<std::endl;
+    }
 }
