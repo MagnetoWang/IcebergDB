9b84946edf94c1421e32d0015189d13121b99c5e
MagnetoWang <1527672216@qq.com>
2019-04-20 22:51:45 +0800

    修改内容：终于把cmake的warning过滤掉了！！！

--- ibdb/compiler.sh
+++ ibdb/compiler.sh
@@ -1,12 +1,14 @@
+#!/usr/bin/env bash
 ROOT_PATH=`pwd`
 
-echo `sh generate-code.sh` 2>/dev/null
+#echo `sh generate-code.sh` 2>/dev/null
 
-
+sh generate-code.sh 2>/dev/null
 mkdir -p build
 cd build
 # cmake -Wdeprecated-declarations=ON -Wno-deprecated=ON ..
-echo `cmake ..` 2>/dev/null
-make -j5 
+#echo ` -Wdeprecated-declarations` 2>/dev/null
+cmake ..
+make -j5 -Wdeprecated-declarations
 
 cd $ROOT_PATH
